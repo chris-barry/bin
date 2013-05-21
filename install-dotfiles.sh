10feec30c82ee996709ce95b2fc4ec10cc8c1f2f
Chris Barry <chris@barry.im>
2013-05-21 00:11:34 -0400

    New script

---
+++ install-dotfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# This script helps to automate configuration of dotfile
+
+# Usage: ./install-dotfiles.sh [list of configuration files]
+
+repo="https://github.com/chris-barry/dotfiles"
+
+if [[ -z $(which git) ]]; then
+	echo "Git is not installed!"
+	exit 1
+fi
+
+cd $HOME
+
+#git clone $repo $HOME/.dotfiles
+
+while [ $# -ne 0 ]
+do
+	if [ -f "$HOME/.dotfiles/$1" ]; then 
+		echo "ln -s $HOME/.dotfiles/$1 $HOME/.$1"
+		#ln -s .dotfile/$1 $HOME/.$1
+	else
+		echo "Cannot find $1"
+	fi
+
+	shift
+done
+
+exit 0
+
