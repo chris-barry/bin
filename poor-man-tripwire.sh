0e5cfaa72c5c562c6e2a958d2b14150055956884
Chris Barry <chris@barry.im>
2014-08-19 00:33:43 -0400

    Various changes

--- poor-man-tripwire.sh
+++ poor-man-tripwire.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
+#
+# poor-man-tripwire.sh - Poor man's tripwire.
+#
+# Author: Chris Barry <chris@barry.im>
+#
+# License: Public domain.
  
-##
-# Poor man's tripwire.
-##
- 
-SUMS=~/sums.orig
-DIR=$HOME
- 
+SUMS=sums.orig
+DIR=$HOME/bin
+
 cd $DIR
- 
-if [ ! -f $SUMS ]; then
-	touch $SUMS
-	find . -type f -exec sha256sum {} > $SUMS +
-	echo "Made new hash file."
-	#chown root:root $SUMS
-	#chmod 755 $SUMS
-	exit
-fi
- 
-find . -type f -exec sha256sum {} > ~/sums.new +
- 
-if [ -z "$(diff $SUMS ~/sums.new)" ]; then
-	echo "All looks good."
-else    
-	echo "You should panic."
-fi
+
+while getopts icd o
+do	case "$o" in
+	d)
+		DIR="$OPTARG"
+		;;
+	i)
+#		touch $SUMS
+		find . -type f -exec sha256sum {} > $SUMS +
+		echo "Made new hash file."
+		#chown root:root $SUMS
+		#chmod 755 $SUMS
+		exit
+		;;
+	c)
+		find . -type f -exec sha256sum {} > ~/sums.new +
+
+		if [ -z "$(diff $SUMS ~/sums.new)" ]; then
+			echo "All looks good."
+		else    
+			echo "You should panic."
+		fi
+		;;
+	*)
+		echo "SUPPLY FLAGS"
+		break
+		;;
+	esac
+done
