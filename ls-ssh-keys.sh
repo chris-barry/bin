efb31b5691d07d25db51824ed255922826986f17
Chris Barry <chris@barry.im>
2014-08-19 00:47:16 -0400

    New script for listing ssh public keys

---
+++ ls-ssh-keys.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# ls-ssh-keys.sh - Lists SSH host keys.
+#
+# Author: Chris Barry <chris@barry.im>
+#
+# License: Public domain.
+
+FILES=$(find /etc/ssh \! -name '*.pub')
+FILES=$(find /etc/ssh \! -name '*.pub' -name 'ssh_host*')
+
+for key in $FILES; do
+	ssh-keygen -lf $key
+done
